838267a587cdb63f8fb520540a9fa2ef3d31193b
Moustafanasr2 <121619554+Moustafanasr2@users.noreply.github.com>
2022-12-30 16:34:13 +0200

    Add files via upload

---
+++ مشروع ١/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace Project_1
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+        
+        Console.Write("Enter num1: ");
+        int num1 = int.Parse(Console.ReadLine());
+        Console.Write("Enter num2: ");
+        int num2 = int.Parse(Console.ReadLine());
+        Console.WriteLine("From  " + num1 + " To " + num2 );
+        for(int M = num1;M <= num2 ; M++ )
+        {
+         int c = 0;
+           for(int j = 2; j <= M / 2 ; j++)
+              if(M % j ==0)
+              {
+              	
+              	 c++;
+              	 break;
+              	
+              	
+              }
+           if(c == 0 && M != 1)
+           {
+           	Console.WriteLine(M);
+           }
+        	
+        	
+        }
+        
+        
+         
+		}
+	}
+}
---
+++ مشروع ٢/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace Project_2
+{
+	public static class Program
+	{
+		public static void Main()
+		{
+   
+   
+        
+           Console.Write("Enter num1: ");
+           int num1 = int.Parse(Console.ReadLine());
+           Console.Write("Enter num2: ");
+           int num2 = int.Parse(Console.ReadLine());
+           for(int M = num1;M <= num2 ;M++)
+           {
+           	 
+           	  if(isperfect(M))
+           	  {
+           	  	Console.WriteLine(M);
+           	  }
+           	 
+           }
+			
+  }	  
+  static bool isperfect(int num)
+  {
+ 	 int sum = 0;
+ 	   for(int M = 1 ; M < num ; M++)
+ 	   {
+ 	  	 if(num % M ==0)
+ 	  	 {
+ 	  		sum += M;
+ 	  	 }
+ 	   }
+ 	   return sum ==num;
+  }
+		   
+   
+   
+   
+   
+   
+   
+   
+		
+	    }
+	  
+    }
+   
+
+
+
+    
